578fba2a1b7018b5c4d29164d916f8755ff2b95a
venganza312 <gpfm312@gmail.com>
2021-11-26 16:51:34 +0900

    initialize eureka-server

---
+++ src/test/java/com/venh/eureka/VenhEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venh.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenhEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
